d6a08e159d48de349210b985f232eb4bce113c5b
Efrain Lemus <efraintlemusm@gmail.com>
2025-03-07 21:23:55 -0700

    add ebitda calculator

---
+++ app/components/globals.js
@@ -0,0 +1,2 @@
+export const Website = 'latinowebstudio.com';
+export const Company = 'Latino Web Studio';
---
+++ app/ebitda-calculator/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { Company, Website } from "../components/globals";
+
+export const metadata: Metadata = {
+  title: `EBITDA Calculator - What Is Your Business Worth? - ${Company}`,
+  description: "Built by Tadeo Martinez. Investor. Entrepreneur. Programmer.",
+  openGraph: {
+    title: `EBITDA Calculator - What Is Your Business Worth? - ${Company}`,
+    description: "Built by Tadeo Martinez. Investor. Entrepreneur. Programmer.",
+    url: `https://${Website}/ebitda-calculator`,
+    type: "website",
+    images: [
+      {
+        url: "/photos/ebitda-calculator.jpg",
+        width: 1200,
+        height: 630,
+        alt: "EBITDA Calculator - What Is Your Business Worth?",
+      },
+    ],
+  },
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
