1ed1c786ea68ea16dc890c18b934a8d9de0bf0a8
Efrain Lemus <efraintlemusm@gmail.com>
2025-03-22 00:48:34 -0600

    attempting to fix meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,13 +21,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Web Design & SEO in Denver, CO - Latino Web Studio",
   description:
-    "From theme updates hosting, DNS, server management to SEO, email marketing and custom designs, we handle all your technical needs.",
+    "Latino Web Studio provides expert web design, SEO, and full-stack tech support for businesses in Denver and nationwide. From DNS and hosting to email marketing and custom design, we handle the tech so you can focus on growth.",
   metadataBase: new URL("https://latinowebstudio.com"), // required for relative OG URLs
 
   openGraph: {
     title: "Web Design & SEO in Denver, CO - Latino Web Studio",
     description:
-      "From theme updates hosting, DNS, server management to SEO, email marketing and custom designs, we handle all your technical needs.",
+      "Latino Web Studio provides expert web design, SEO, and full-stack tech support for businesses in Denver and nationwide. From DNS and hosting to email marketing and custom design, we handle the tech so you can focus on growth.",
     url: "https://latinowebstudio.com",
     siteName: "Latino Web Studio",
     images: [
